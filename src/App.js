3569a973469baa478bc8c12ffa6183953047b7d5
Rodgers <wanyonyirodgers11.com>
2022-10-09 14:40:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import  {Switch}  from "react-router";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
